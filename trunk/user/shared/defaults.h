d5eac5aa46301ce39dab6b563c76c81d21a891a5
caomusan <136090704+caomusan@users.noreply.github.com>
2023-06-12 09:42:49 +0800

    Update defaults.h

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -35,8 +35,8 @@
 #define DEF_LAN_DHCP_END	"192.168.10.244"
 #define DEF_LAN_MASK		"255.255.255.0"
 
-#define DEF_WLAN_2G_CC		"CN"
-#define DEF_WLAN_5G_CC		"CN"
+#define DEF_WLAN_2G_CC		"US"
+#define DEF_WLAN_5G_CC		"US"
 #define DEF_WLAN_2G_SSID	"Finer_%s"
 #define DEF_WLAN_5G_SSID	"Finer5G_%s"
 #define DEF_WLAN_2G_GSSID	"PDCN_GUEST"
